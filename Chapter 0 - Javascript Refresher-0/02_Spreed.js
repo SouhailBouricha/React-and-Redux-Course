f7d71458a556b409d83cb5d079c3526a7059b8e0
souhail bouricha <souhailbourigha@gmail.com>
2022-12-15 19:37:46 +0100

    Javascript Refresher

---
+++ Chapter 0 - Javascript Refresher-0/02_Spreed.js
@@ -0,0 +1,7 @@
+const ini_users = ["Souhail", "Oli"];
+const current_users = [...ini_users];
+
+current_users.push("lol");
+
+console.log(current_users);
+console.log(ini_users);
---
+++ Chapter 0 - Javascript Refresher-0/03_Arrow_Functions.js
@@ -0,0 +1,4 @@
+const sayHi = (name) => `Hey ${name}`;
+const greeting = sayHi("Sou");
+
+console.log(greeting);
