5b459855c3efd66ef5b8fabb61a9d8a20ef646e6
souhail bouricha <souhailbourigha@gmail.com>
2022-12-15 19:50:39 +0100

    Javascript Refresher

---
+++ Chapter 0 - Javascript Refresher-0/05_Map_and_Filter.js
@@ -0,0 +1,9 @@
+const users = [
+    { name : "Sou", age : 21},
+    { name : "Gdf", age : 11},
+    { name : "Fdr", age : 31},
+]
+
+const modUsers = users.map(user => user.age);
+
+console.log(modUsers);
