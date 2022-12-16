dee77c89a74dcc5b8036f5aebe56c54f307e938d
souhail bouricha <souhailbourigha@gmail.com>
2022-12-16 17:12:10 +0100

    Javascript Refresher

---
+++ Chapter 1 - The Basics/01_Why_Learn_React.js
@@ -0,0 +1,4 @@
+// 01 - Create Web Apps 
+// 02 - Give a structur to your code (Rules) unlike vanila Js
+// 03 - Stop creating html pages insted of that react will generate html and data for you  
+// 04 -  performens benefits (No Refresh)  
