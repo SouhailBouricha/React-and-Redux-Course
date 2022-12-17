8cbda5eb6760f77d5542b868026f46ff1bc4771f
souhail bouricha <souhailbourigha@gmail.com>
2022-12-17 22:07:48 +0100

    Core Concepts

--- Chapter 2 - Core Concepts/01_props_difinition/src/App.js
+++ Chapter 2 - Core Concepts/01_props_difinition/src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
+import CreateTwitte from './components/CreateTwitte';
 function App() {
   return (
     <div className="App">
       <h1>New Twitter</h1>
+      <CreateTwitte />
     </div>
   );
 }
--- Chapter 2 - Core Concepts/01_props_difinition/src/components/CreateTwitte.js
+++ Chapter 2 - Core Concepts/01_props_difinition/src/components/CreateTwitte.js
@@ -1,5 +1,10 @@
 function CreateTwitte(){
     return (
-        
+        <div className="addTwiite">
+            <textarea cols="50" rows="5"></textarea>
+            <button>Submit</button>
+        </div>
     )
 }
+
+export default CreateTwitte
