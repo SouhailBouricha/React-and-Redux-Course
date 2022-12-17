5360075cd5a9673fb3d5790baf0e772f0567326e
souhail bouricha <souhailbourigha@gmail.com>
2022-12-17 22:05:27 +0100

    Core Concepts

--- Chapter 2 - Core Concepts/01_props_difinition/src/App.js
+++ Chapter 2 - Core Concepts/01_props_difinition/src/App.js
@@ -1,8 +1,8 @@
-import "App.css";
+import "./App.css";
 function App() {
   return (
     <div className="App">
-      
+      <h1>New Twitter</h1>
     </div>
   );
 }
---
+++ Chapter 2 - Core Concepts/01_props_difinition/src/components/CreateTwitte.js
@@ -0,0 +1,5 @@
+function CreateTwitte(){
+    return (
+        
+    )
+}
