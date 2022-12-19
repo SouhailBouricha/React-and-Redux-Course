87b9edcf1f0d863e09f15c0a6c467007673d6b06
souhail bouricha <souhailbourigha@gmail.com>
2022-12-19 17:52:46 +0100

    Core Concepts

--- Chapter 2 - Core Concepts/01_props_difinition/src/components/ListTwittes.js
+++ Chapter 2 - Core Concepts/01_props_difinition/src/components/ListTwittes.js
@@ -3,7 +3,7 @@ import Twitte from "./Twiite";
 function ListTwittes({name,message,setName,twitte,setTwitte,author}){
     return (
         <div className="listTwittes">
-            {twitte.map((t) => <Twitte twittes={twitte} twitte={t} author={author}/>)}
+            {twitte.map((t) => <Twitte twittes={twitte} twitte={t} author={author} setTwitte={setTwitte}/>)}
         </div>
     )
 }
