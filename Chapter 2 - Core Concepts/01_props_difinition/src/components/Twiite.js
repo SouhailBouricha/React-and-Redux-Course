f2b6adb02ffd74ccc50540a87dc3025c6bdeadab
souhail bouricha <souhailbourigha@gmail.com>
2022-12-19 19:10:53 +0100

    Core Concepts

--- Chapter 2 - Core Concepts/01_props_difinition/src/components/Twiite.js
+++ Chapter 2 - Core Concepts/01_props_difinition/src/components/Twiite.js
@@ -1,21 +1,25 @@
 import styled from 'styled-components'
 
-const twitteStyleDiv = styled.div`
-    background-color: red;
+const StyleDiv = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: palevioletred;
+  margin: 0.5em 1em;
+  padding: 0.25em 1em;
 `;
-
 function Twitte({name,message,setName,twitte,setTwitte,author,twittes}){
     const chanchName = () =>{ setName("Bouricha") }
     const suprimme = () =>{
         setTwitte(twittes.filter(t => t.id !== twitte.id));
     }
     return (
-        <twitteStyleDiv className="twitte">
+        <StyleDiv>
             <h1 onClick={chanchName}>{author}</h1>
             <h2>{twitte.message}</h2>
             <button onClick={suprimme}>Delete</button>
             <button>Modify</button>
-        </twitteStyleDiv>
+        </StyleDiv>
     )
 }
 export default Twitte;
