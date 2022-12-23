c7d8c58dbce2625e7550be79b7ce569d3e4850fa
souhail bouricha <souhailbourigha@gmail.com>
2022-12-23 20:38:05 +0100

    Capture Portofolio

---
+++ Chapter 4 - Capture Portofolio/capture/src/components/FaqSection.js
@@ -0,0 +1,39 @@
+import home1 from '../img/home1.png';
+function FaqSection() {
+    return (
+      <div className='faq'>
+        <h2>Any Questions <span>FAQ</span></h2>
+        <div className="question">
+            <h4>HOW DO I START?</h4>
+            <div className="answer">
+                <p>Lorem ipsum dolor sit amet.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, animi! </p>
+            </div>
+        </div>
+        <div className="question">
+            <h4>Daily Schedule</h4>
+            <div className="answer">
+                <p>Lorem ipsum dolor sit amet.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, animi! </p>
+            </div>
+        </div>
+        <div className="question">
+            <h4>Diffrent Payment Methods</h4>
+            <div className="answer">
+                <p>Lorem ipsum dolor sit amet.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, animi! </p>
+            </div>
+        </div>
+        <div className="question">
+            <h4>What Products Do You Offer</h4>
+            <div className="answer">
+                <p>Lorem ipsum dolor sit amet.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, animi! </p>
+            </div>
+        </div>
+      </div>
+    );
+}
+  
+export default FaqSection;
+  
--- Chapter 4 - Capture Portofolio/capture/src/pages/AboutUs.js
+++ Chapter 4 - Capture Portofolio/capture/src/pages/AboutUs.js
@@ -1,10 +1,12 @@
 import AboutSection from "../components/AboutSection";
 import ServicesSection from "../components/ServicesSection";
+import FaqSection from "../components/FaqSection";
 function AboutUs() {
     return (
       <>
         <AboutSection/>
         <ServicesSection/>
+        <FaqSection/>
       </>
     );
 }
