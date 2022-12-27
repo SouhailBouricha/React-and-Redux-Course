1d33588d91f1a68ae2673299296c608775b93dbb
souhail bouricha <souhailbourigha@gmail.com>
2022-12-27 20:16:58 +0100

    Async and Redux

--- Chapter 5 - Async and Redux/redux/src/store/index.js
+++ Chapter 5 - Async and Redux/redux/src/store/index.js
@@ -1,7 +1,15 @@
 import { createStore } from "redux";
 
-const reducerFun = (state = {counter : 0}, action) =>{
-
+const reducerFun = (state = {counter : 10}, action) =>{
+    if(action.type === "INC"){
+        return { counter : state.counter + 1 };
+    }
+    if(action.type === "DESINC"){
+        return { counter : state.counter - 1 };
+    }
+    return state;
 }
 
 const store = createStore(reducerFun);
+
+export default store;
