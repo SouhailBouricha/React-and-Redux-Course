63eabb7908b9fd4893399be7fd1fac0e0c5e053f
souhail bouricha <souhailbourigha@gmail.com>
2022-12-27 20:32:49 +0100

    Async and Redux

--- Chapter 5 - Async and Redux/redux/src/App.js
+++ Chapter 5 - Async and Redux/redux/src/App.js
@@ -1,15 +1,27 @@
 import './App.css';
 import { useSelector,useDispatch } from 'react-redux';
+import { useRef } from 'react';
 function App() {
   const counter = useSelector((state) => state.counter);
-  const Increment = () => useDispatch({type: "INC"});
-  const Desincrement = () => useDispatch({type: "DESINC"});
+  const dispatch = useDispatch();
+  const inpu = useRef(null);
+  const Increment = () => {
+    dispatch({type: "INC"});
+  }
+  const Desincrement = () => { 
+    dispatch({type: "DESINC"});
+  }
+  const Addby = () => { 
+    dispatch({type: "ADDBY",pyload : Number(inpu.current.value) ? Number(inpu.current.value) : 0});
+  }
   return (
     <div className="App">
       <h1>counter app</h1>
       <h2>{counter}</h2>
+      <input ref={inpu} type="text"/>
       <button onClick={Increment}>Increment</button>
       <button onClick={Desincrement}>Desincrement</button>
+      <button onClick={Addby}>Add</button>
     </div>
   );
 }
--- Chapter 5 - Async and Redux/redux/src/store/index.js
+++ Chapter 5 - Async and Redux/redux/src/store/index.js
@@ -1,11 +1,14 @@
 import { createStore } from "redux";
 
-const reducerFun = (state = {counter : 10}, action) =>{
+const reducerFun = (state = {counter : 0}, action) =>{
     if(action.type === "INC"){
         return { counter : state.counter + 1 };
     }
     if(action.type === "DESINC"){
         return { counter : state.counter - 1 };
+    }
+    if(action.type === "ADDBY"){
+        return { counter : state.counter + action.pyload };
     }
     return state;
 }
