0deb28ac89bcb8856eecefb20d31dfc56d6f1005
souhail bouricha <souhailbourigha@gmail.com>
2022-12-29 01:54:43 +0100

    Async and Redux

--- Chapter 5 - Async and Redux/Redux-Shopping-Cart-App-StartingApp/src/App.js
+++ Chapter 5 - Async and Redux/Redux-Shopping-Cart-App-StartingApp/src/App.js
@@ -2,27 +2,43 @@ import React, { useEffect } from "react";
 import "./App.css";
 import Auth from "./components/Auth";
 import Layout from "./components/Layout";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import CartItems from "./components/CartItems";
 import Notifications from "./components/Notifications";
+import { uiAction } from "./store/ui-slicer";
+let firstRunder = true;
 function App() {
   const AuthState = useSelector(state => state.auth.isLogede);
   const cartState = useSelector(state => state.cart.itemslist);
   const showCart = useSelector(state => state.cart.showCart);
+  const notification = useSelector(state => state.ui.notification);
+  const dispatch = useDispatch();
   const cart = useSelector(state => state.cart);
   useEffect(()=>{
+    if(firstRunder){
+      firstRunder = false;
+      console.log(cart); 
+      return;
+    }
+    dispatch(uiAction.showNotif({message : "Sending Request" , type : "warning", open : true}));
     const sendData = async () => {
       const data = await fetch("https://redux-65dc8-default-rtdb.firebaseio.com/cartItems.json",{
         method : "PUT",
         body : JSON.stringify(cart)
       });
       const response = data.json();
+      dispatch(uiAction.showNotif({message : "Sending Request To Data Base Succsessfully" , type : "success", open : true}));
     }
-    sendData();
+    sendData().catch(err =>{
+      dispatch(uiAction.showNotif({message : "Sending Request To Data Base Failed" , type : "error", open : true}));
+    });
   },[cart])
   return (
     <div className="App">
-      <Notifications type={"error"} message={"hi"}></Notifications>
+      {
+        notification &&
+      <Notifications type={notification.type} message={notification.message}></Notifications>
+      }
     {
       (!AuthState && <Auth />)
     }
--- Chapter 5 - Async and Redux/Redux-Shopping-Cart-App-StartingApp/src/components/Notifications.js
+++ Chapter 5 - Async and Redux/Redux-Shopping-Cart-App-StartingApp/src/components/Notifications.js
@@ -1,11 +1,21 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Alert } from "@mui/material";
-const Notifications = ({type, message}) => {
+import { uiAction } from "../store/ui-slicer";
 
+const Notifications = ({type, message}) => {
+  const notification = useSelector(state => state.ui.notification);
+  const dispatch = useDispatch();
+
+  const handelClose  = () =>{
+    dispatch(uiAction.showNotif({ open : false }));
+  }
   return (
     <div>
-      <Alert severity={type}>{message}</Alert>
+      {
+        notification.open && 
+      <Alert severity={type} onClose={handelClose}>{message}</Alert>
+      }
     </div>
   );
 };
